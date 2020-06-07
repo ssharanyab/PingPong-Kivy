63d3b33a09a7a60f514b7178e1538d03f1bb0a73
Sharanya-Bharghavi <ssharanyab@gmail.com>
2020-06-07 18:25:56 +0530

    commit

--- PingPong/main.py
+++ PingPong/main.py
@@ -5,8 +5,10 @@
 
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
 
 class PongBall(Widget):
@@ -20,12 +22,21 @@ class PongBall(Widget):
 
 
 class PongGame(Widget):
-    pass
+    ball = ObjectProperty(None)
+
+    def serve_ball(self):
+        self.ball.velocity = Vector(6, 0).rotate(randint(0, 360))
+
+    def update(self, dt):
+        self.ball.move()
 
 
 class PongApp(App):
     def build(self):
-        return PongGame()
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 80.0)
+        return game
 
 
 PongApp().run()
